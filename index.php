9359b1dc5fdce17d0aeb18d3d804fbf083ba600b
Xavi <xavi15t@gmail.com>
2015-03-28 23:15:43 +0100

    commit final

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Registration</title>
+    <title>Inscriure Oferta</title>
 
     <!-- Angular JS -->
     <script src="js/angular.min.js"></script>
@@ -35,12 +35,26 @@
     <![endif]-->
   </head>
   <body>
+
+<header>
+    <div class="container">
+        <div class="wally-logo">
+            <img src="img/wallyJobsLogoHor.svg" alt="WallyJobs"/>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+</header>
+
     <div class="container" ng-app="hackathonApp" ng-controller="InscriptionController">
       <div class="row">
+
+    <article class="apply-offer">
         <div class="col-xs">
-          <h2>{{job.title}}</h2>
+
+        <h4 class="apply-offer-subtitle">Inscriure's en l'oferta</h4>
+        <h2 class="apply-offer-title">{{job.title}}</h2>
           <?php
-          $url_fb = "https://apisocial.wallyjobs.com/login/facebook?urlOK=".urlencode("https://applicant.wallyjobs.com/");
+            $url_fb = "https://apisocial.wallyjobs.com/login/facebook?urlOK=".urlencode("https://applicant.wallyjobs.com/");
           ?>
 
           <div class="error">S'han d'emplenar tots els camps</div>
@@ -49,21 +63,29 @@
           </div>
 
           <form enctype="multipart/form-data" method="post" action="form.php" data-toggle="validator" role="form">
-            <br>
-              <textarea class="form-control" rows="5" id="description" name="description" placeholder="Desar carta de presentació"></textarea>
-            <br>
-            Currículum vitae:<br>
+            <div class="form-group apply-offer-presentation">
+              <label class="control-label" for="apply-offer-presentation">Carta de presentació</label>
+              <textarea class="form-control" rows="8" id="description" name="description" placeholder="La teva carta de presentació"></textarea>
+            </div>
+            <div class="form-group apply-offer-cv">
+                <label class="control-label" for="apply-offer-cv">Currículum Vitae</label>
+                <input class="form-control" id="userfile" type="file" name="userfile" accept="application/pdf">
+            </div>
             <input type="hidden" value="{{user_id}}" id="user_id" name="user_id" />
             <input type="hidden" value="{{job.id}}" id="job_id" name="job_id" />
             <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-            <input type="file" id="userfile" name="userfile" accept="application/pdf">
-            <br>
-            <br>
-            <input type="submit" name="submit" id="submit" value="Inscriure'm">
+            <div class="form-group apply-offer-submit">
+                <a onclick="window.history.back();" class="btn btn-link">Cancel·la</a>
+                <button type="submit" name="submit" id="submit" class="btn btn-primary">Aplica</button>
+            </div>
           </form>
         </div>
+    </article>
       </div>
     </div>
+    <footer>
+      <p class="small">©2015 Bla bla bla, ...</p>
+    </footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-1.11.2.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
