a78b8d64098a650c067be5f5ff2cb057298370cb
Xavi <xavi15t@gmail.com>
2015-03-28 17:17:08 +0100

    recomanacio

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 var myApp = angular.module('hackathonApp',[]);
 
 myApp.controller('InscriptionController', ['$scope', '$http', '$location', function($scope, $http, $location) {
-	$scope.data = {};
+	$scope.job = {};
 
 	$scope.register = function(form) {
    		window.location.href = 'recommend.html';
@@ -15,14 +15,25 @@ myApp.controller('InscriptionController', ['$scope', '$http', '$location', funct
 
 	token = "1";//integracio
 
-  	$http.post({url:"http://apic.wallyjobs.com/user/"+token})
-           .success(function(response) {$scope.items = response;});
+  	$http.get("http://apic.wallyjobs.com/jobs/" + token)
+           .success(function(response) {
+           	$scope.job = response;
+        });
 
 }]);
 
 myApp.controller('RecommendController', ['$scope', '$http', function($scope, $http) {
-  $http.post({url:"http://apic.wallyjobs.com/jobs/" + token})
-           .success(function(response) {$scope.items = response;});
+	$scope.job = {};
+
+	var url = $location.absUrl();
+	var token = url.split('/')[url.split('/').length-1];
+
+	token = "1";//integracio
+
+  	$http.get("http://apic.wallyjobs.com/jobs/" + token)
+           .success(function(response) {
+           	$scope.job = response;
+        });
 }]);
 
 var register = function(form) {
